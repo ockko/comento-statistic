9e782b332d5acfcc54425b20c7bd72bb308c3c2f
ockko <ocko119@gmail.com>
2026-02-05 01:05:50 +0900

    3차 과제

---
+++ src/main/java/com/demo/comentoStatistic/dto/YearCountDto.java
@@ -0,0 +1,9 @@
+package com.demo.comentoStatistic.dto;
+
+import lombok.Getter;
+
+@Getter
+public class YearCountDto {
+    private String year;
+    private long totCnt;
+}
---
+++ src/main/java/com/demo/comentoStatistic/dto/YearMonthCountDto.java
@@ -0,0 +1,9 @@
+package com.demo.comentoStatistic.dto;
+
+import lombok.Getter;
+
+@Getter
+public class YearMonthCountDto {
+    private String yearMonth;
+    private long totCnt;
+}
